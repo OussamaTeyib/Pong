f6c91d42bd54df1a2746d92c3fb33bae04784381
Oussama Teyib <oussama.teyib@gmail.com>
2024-12-27 22:41:57 +0100

    Add MoveBall()

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@ typedef struct {
     float x, y;
     float radius;
     int velocity;
+    int x_direction, y_direction;
 } Ball;
 
 Rectangle paddles[2];
@@ -15,11 +16,12 @@ Ball ball;
 
 void UpdateDrawFrame(void);
 void MovePaddle(int id, int direction);
+void MoveBall(void);
 
 int main(void) {
     InitWindow(screenWidth, screenHeight, "Pong");
 
-    ball = (Ball) {screenWidth / 2, screenHeight / 2, 10.0f, 5.0f};
+    ball = (Ball) {screenWidth / 2, screenHeight / 2, 10.0f, 5.0f, 1, 1};
 
     paddles[0] = (Rectangle) {0.0f, (screenHeight - screenHeight / 8) / 2, 10.0f, screenHeight / 8};
     paddles[1] = (Rectangle) {screenWidth - 10.0f, (screenHeight - screenHeight / 8) / 2, 10.0f, screenHeight / 8};
@@ -41,12 +43,14 @@ void UpdateDrawFrame(void) {
     // Update
     if (IsKeyDown(KEY_S))
         MovePaddle(0, -1);
-    if (IsKeyDown(KEY_Z))
+    if (IsKeyDown(KEY_Z ))
         MovePaddle(0, 1);
     if (IsKeyDown(KEY_UP))
         MovePaddle(1, -1);
     if (IsKeyDown(KEY_DOWN))
         MovePaddle(1, 1);
+
+    MoveBall();
 
     // Begin the drawing process
     BeginDrawing();
@@ -70,4 +74,14 @@ void MovePaddle(int id, int direction) {
 
     if (paddles[id].y < 0) paddles[id].y = 0;
     if (paddles[id].y > (screenHeight - paddles[id].height)) paddles[id].y = screenHeight - paddles[id].height;
+}
+
+void MoveBall(void) {
+    ball.x += ball.velocity * ball.x_direction;
+    ball.y += ball.velocity * ball.y_direction;
+
+    if ((ball.x + ball.radius) >= screenWidth || (ball.x - ball.radius) <= 0)
+        ball.x_direction *= -1;
+    if ((ball.y + ball.radius) >= screenHeight || (ball.y - ball.radius) <= 0)
+        ball.y_direction *= -1;
 }
